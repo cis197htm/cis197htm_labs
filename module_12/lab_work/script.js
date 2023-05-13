21aae96ad8ab6e30c24951594c7046925dff8901
Olga Kapora <o.kapora1@saints.mhcc.edu>
2023-05-12 22:19:45 -0700

    Module 12 Done

---
+++ module_12/lab_work/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Olga Kapora
+    File Name: script.js
+    Date: 5/12/23
+*/
+
+//jQuery for hero image to consume the header window space
+$(document) .ready(function(){
+    $('.hero'). height($(window).height());
+})
