ebf5a5ae4aa68877bf32128da0d394205a9553e8
Olga Kapora <o.kapora1@saints.mhcc.edu>
2023-05-12 21:52:23 -0700

    Module 12 Done

--- module_12/sample_project/fitness-bootstrap/scripts/script.js
+++ module_12/sample_project/fitness-bootstrap/scripts/script.js
@@ -1,7 +1,10 @@
 /*
-    Student Name: Maria Cruz
+    Student Name: Olga Kapora
     File Name: script.js
-    Date: May 25, 2022
+    Date: 5/12/23
 */
 
 //jQuery for hero image to consume the header window space
+$(document) .ready(function(){
+    $('.hero'). height($(window).height());
+})
